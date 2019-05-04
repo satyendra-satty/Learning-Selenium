b4740e46aae975e06ad9b63e7e7d88fcaf0bc155
satyendrasinghrajpoot <satyendrasinghrajpoot@192.168.0.13>
2019-05-04 22:58:33 +0200

    My Fisrt commmit

---
+++ LearningJavaLanguage/src/LeaningJava/MyFirstJavaProgram.java
@@ -0,0 +1,21 @@
+package LeaningJava;
+
+	
+
+public class MyFirstJavaProgram {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 90;
+		int y = 100;
+		System.out.print("value of x  "+x);
+		System.out.print("  value of y  "+y);
+		
+		
+	}
+	
+	
+}
