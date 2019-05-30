9dceb1faf28860e462e2b5997ae07f16ff8a97e9
satyendrasinghrajpoot <satyendrasinghrajpoot@192.168.0.13>
2019-05-30 21:29:22 +0200

    Adding Logger Configuration file

--- LearningJavaLanguage/src/LeaningJava/MyFirstJavaProgram.java
+++ LearningJavaLanguage/src/LeaningJava/MyFirstJavaProgram.java
@@ -1,6 +1,7 @@
 package LeaningJava;
 
-	
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class MyFirstJavaProgram {
 
@@ -8,11 +9,15 @@ public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	   Logger Logger=org.apache.log4j.Logger.getLogger("MyFirstJavaProgram");
+	   PropertyConfigurator.configure("Log4j.properties");
 
 		int x = 80;
 		int y = 100;
 		System.out.print("value of x  "+x);
+		Logger.info("value of x is prineted");
 		System.out.print("  value of y  "+y);
+		Logger.info("value of x is prineted");
 		System.out.print("First Commit successfull");
 		
 	}
