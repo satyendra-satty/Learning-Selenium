c8fa6a09ffab050e29b5e10c333d97bbafa18da4
satyendrasinghrajpoot <satyendrasinghrajpoot@192.168.0.13>
2019-05-30 20:12:57 +0200

    Second comint

--- LearningJavaLanguage/src/LeaningJava/MyFirstJavaProgram.java
+++ LearningJavaLanguage/src/LeaningJava/MyFirstJavaProgram.java
@@ -9,7 +9,7 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int x = 90;
+		int x = 80;
 		int y = 100;
 		System.out.print("value of x  "+x);
 		System.out.print("  value of y  "+y);
